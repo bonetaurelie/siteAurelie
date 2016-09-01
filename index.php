dca0da0d4e4d158564a01691669bc9749d736a36
bonetaurelie <bonetaurelie@yahoo.fr>
2016-09-01 08:17:27 +0200

    création index

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Aurélie BONET</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+
+<?php
+include ('menu.php');
+?>
+
+<div class="container">
+    <div class="row">
+        <div class="index text-center">
+            <img src="images/photoidentiteaureliebonet.jpg" alt="photo identité" class="img-circle">
+            <blockquote>"La vie n'est pas un long un fleuve tranquille."</blockquote><br/>
+            <p class="text-center">Cette phrase a un sens tout particulier pour moi car depuis mon départ de l'Armée, il a fallu que je fasse preuve de motivation et de persévérance  pour me reconvertir à chaque fois que le besoin s'en faisait ressentir.<br/>
+                Aujourd'hui, mon but est atteint, je suis en voie de <strong>devenir développeuse web</strong> et mon souhait est de <strong>m'épanouir dans ce domaine</strong>.<br/>
+                Mon profil vous intéresse, poursuivez la visite...</p>
+        </div>
+    </div>
+</div>
+
+<?php
+include ('footer.php');
+?>
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
