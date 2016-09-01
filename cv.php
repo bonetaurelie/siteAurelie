c716f35b14fe3f8576e0cf9856e98d676cabba50
bonetaurelie <bonetaurelie@yahoo.fr>
2016-09-01 08:24:53 +0200

    création contenu cv

---
+++ cv.php
@@ -0,0 +1,192 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Site du village - Activités du mois</title>
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+
+<body>
+
+<?php
+include ('menu.php');
+?>
+
+<div class="container">
+    <div class="presentation">
+        <div class="text-center2">
+            <p>La vie n'est pas un long fleuve tranquille, il est donc important de savoir réagir et de s'adapter aux aléas de la vie.</p>
+            <p>Telle est ma conception de la vie aujourd'hui, surtout professionnelle.</p>
+            <p>Depuis mon départ de l'Armée de Terre, j'ai su m'adapter et me former afin de développer mes compétences professionnelles.</p>
+            <p>Je suis une personne motivée, qui sait se rendre disponible quand cela est nécessaire et qui s'investit dans son travail.</p>
+        </div>
+    </div>
+
+    <div class="cv">
+        <div class="row">
+            <div class="panel panel-default col-lg-6 col-md-6">
+                <div class="panel-heading">
+                    <div class="panel-title"><img src="images/icone experience.png">Expérience</div>
+                </div>
+                <div class="panel-body experience">
+                    <ul>
+                        <li><b>21012-mars 2014 :</b> Poste de vendeuse et de pâtissière à La Pétrie de Kilian à Pernes les Fontaines.</li>
+                        <li><p><b>Setpembre 2010-Septembre 2011 :</b> Assistante Marketing et commerciale sédentaire à Jeux de Lumière, Avignon en alternance.</p>
+                            <p>Durant mon contrat, j'ai réfléchi sur la stratégie pour le développement de l'activité de la société, prospecté de la clientèle, suivi et relancé des dossiers mais aussi effectué du conseil et de la vente.</p></li>
+                        <li><p><b>Février à Avril 2009 :</b> Stagiaire en communication à la Communauté de Communes de la CoVe à Carpentras.</p>
+                            <p>J'ai piloté le projet du site portail de la CoVe et la réalisation de son contenu mais aussi élaboré divers dossiers de presse</p></li>
+                        <li><p><b>Octobre 2009 :</b> Stagiaire de communcation à Sécuriforce à Carpentras</p>
+                            <p>Durant ce stage, j'ai conçu un plan de conception, prospecté des prestataires et effectué une budgétisation du plan de communication</p></li>
+                        <li><p><b>Mars 2005 - Février 2009 :</b> Militaire du Rang au 4ème Régiment du Matériel à Nîmes.</p>
+                            <p>Pendant mon engagement j'ai tenu divers postes de secrétaire : DRH, BML et gestion de personnel.</p></li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="panel panel-default col-lg-6 col-md-6">
+                <div class="panel-heading">
+                    <div class="panel-title"><img src="images/icone competences.png">Compétences</div>
+                </div>
+                <div class="panel-body competences">
+                    <p>HTML5/CSS3</p>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-default progress-bar-striped" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 75%">
+                        </div>
+                    </div>
+
+                    <p>Bootstrap</p>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-default progress-bar-striped" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
+                        </div>
+                    </div>
+
+                    <p>Javascript/Jquery</p>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-default progress-bar-striped" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+                        </div>
+                    </div>
+
+                    <p>PHP/MySQL</p>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-default progress-bar-striped" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 70%">
+                        </div>
+                    </div>
+
+                    <p>Symfony2</p>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-default progress-bar-striped" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width:50%">
+                        </div>
+                    </div>
+
+                    <p>Git</p>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-default progress-bar-striped" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width:45%">
+                        </div>
+                    </div>
+
+                    <p>Wordpress</p>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-default progress-bar-striped" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+                        </div>
+                    </div>
+
+                    <p>Adobe Photoshop</p>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-default progress-bar-striped" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 55%">
+                        </div>
+                    </div>
+
+                    <p>Anglais</p>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-default progress-bar-striped" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
+                        </div>
+                    </div>
+
+                    <p>Capacité à travailler en équipe</p>
+                    <div class="progress">
+                        <div class="progress-bar progress-bar-default progress-bar-striped" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 85%">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="eductation panel panel-default col-lg-6 col-md-6">
+                <div class="panel-heading">
+                    <div class="panel-title"><img src="images/icone education.png">Education</div>
+                </div>
+                <div class="panel-body education">
+                    <ul>
+                        <li><b>2015 - 2016 :</b> Formation en vue d'obtenir le titre professionnel de niveau II de Chef de projet Multimédia - Développement chez Openclassrooms</li>
+                        <li><b>2012 - 2013 :</b> CAP Pâtisserie</li>
+                        <li><b>2010 - 2011 :</b> DEES Marketing préparé en alternance à Eurexia Avignon.
+                            <p><b><i><u>Enseignements suivis:</u></i></b> Europe, Anglais, Markeitng</p></li>
+                        <li><b>2008 - 2010 :</b> BTS Communication des entreprises au CNED.
+                            <p><b><i><u>Enseignements suivis:</u></i></b> Stratégie de communication, Management, Droit, Espagnol, Anglais</p></li>
+                        <li><b>2003 - 2004 :</b> 1ère année DEUG AES à l'Université d'Avignon.
+                            <p><b><i><u>Enseignements suivis:</u></i></b>Economie, Droit, Sociologie, Anglais</p></li>
+                        <li><b>2002 :</b> Baccalauréat littéraire.</li>
+                    </ul>
+                </div>
+            </div>
+
+            <div class="panel panel-default col-lg-6 col-md-6">
+                <div class="panel-heading">
+                    <div class="panel-title"><img src="images/icone interet.png">Intérêts</div>
+                </div>
+                <div class="panel-body interet">
+                    <div class="bordure col-lg-6 col-lg-offset-3 col-md-8 col-md-pull-2 col-sm-6 col-sm-offset-3 col-xs-6 col-xs-offset-3">
+                        <div class="cercle">
+                            <div class="cercle1">
+                                Lecture
+                            </div>
+                            <div class="cercle2">
+                                Piano
+                            </div>
+                            <div class="cercle3">
+                                Voyager
+                            </div>
+                            <div class="cercle4">
+                                Sport
+                            </div>
+                            <div class="cercle5">
+                                Musique
+                            </div>
+                            <div class="cercle6">
+                                Pâtisserie
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row col-lg-6 col-lg-push-2 col-md-6 col-md-push-2 col-sm-8 col-sm-push-1 col-xs-8 col-xs-push-1">
+            <a href="images/CV_developpeur_web.pdf" target="_blank">Mon cv à télécharger</a>
+
+        </div>
+    </div>
+</div>
+
+<?php
+include ('footer.php');
+?>
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
