795fa0836a1e66b188682a08ea669566e50cd76d
bonetaurelie <bonetaurelie@yahoo.fr>
2016-09-02 10:37:08 +0200

    mise à jour présentation

--- index.php
+++ index.php
@@ -31,10 +31,11 @@ include ('menu.php');
     <div class="row">
         <div class="index text-center">
             <img src="images/photoidentiteaureliebonet.jpg" alt="photo identité" class="img-circle">
-            <blockquote>"La vie n'est pas un long un fleuve tranquille."</blockquote><br/>
-            <p class="text-center">Cette phrase a un sens tout particulier pour moi car depuis mon départ de l'Armée, il a fallu que je fasse preuve de motivation et de persévérance  pour me reconvertir à chaque fois que le besoin s'en faisait ressentir.<br/>
-                Aujourd'hui, mon but est atteint, je suis <strong>développeuse web</strong> et mon souhait est de <strong>m'épanouir dans ce domaine</strong>.<br/>
-                Mon profil vous intéresse, poursuivez la visite...</p>
+            <span class="dev"><blockquote><b>"La vie n'est pas un long un fleuve tranquille."</b></blockquote></span><br/>
+            <p class="accueiltext-center">Cette phrase a un sens tout particulier pour moi car depuis mon départ de l'Armée, il a fallu que je fasse preuve de <span class="dev"><strong>motivation</strong></span> et de <span class="dev"><strong>persévérance</strong></span>  pour me reconvertir à chaque fois que le besoin s'en faisait ressentir.<br/><br/>
+                Aujourd'hui, mon but est atteint, je suis <span class="dev"><strong>développeuse web</strong></span>  et mon souhait est de <strong>m'épanouir dans ce domaine</strong>.<br/><br/>
+                Mon domaine de compétences s'étend du <span class="dev"><strong>HTML5/CSS3</strong></span> au <span class="dev"><strong>PHP</strong></span>, <span class="dev"><strong>Symfony 2 et 3</strong></span>, <span class="dev"><strong>IONIC</strong></span>, à <span class="dev"><strong>la gestion de projet</strong></span> et à bien d'autres domaines que vous découvrirez si vous consultez <a href="cv.php">mon cv</a>.<br/><br/>
+                Mon profil vous intéresse <a href="contact.php">contactez-moi</a> et poursuivez la visite ...</p>
         </div>
     </div>
 </div>
