6f7c8c8b5e1cff411bf615121d3b5b250a82f302
bonetaurelie <bonetaurelie@yahoo.fr>
2016-09-03 14:25:46 +0200

    création de la page à propose de moi

---
+++ aProposDeMoi.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content=" Aurélie Bonet, développeuse, chef de projet, chef de projet multimédia, Vaucluse, Provence Alpes Côte d'Azur, PACA, développeur, concepteur, programmeur, conceptrice, programmeuse"/>
+    <meta name="description" content="Aurélie Bonet, développeuse web junior dont le dommaine de compétences est assez vaste et qui est à la recherche d'un emploi."/>
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Aurélie BONET, développeuse</title>
+
+    <!-- Bootstrap -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="shortcut icon" type="image/x-icon" href="images/x-icon/faviconBis.ico" />
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body  itemscope itemtype="http://schema.org/WebPage">
+
+
+<?php
+include ('menu.php');
+?>
+<div class="container">
+    <div class="row">
+        <div class="index text-center">
+            <h1 itemprop="name">Aurélie Bonet, Développeuse - Chef de projet junior</h1>
+            <img itemprop="image" src="images/photoidentiteaureliebonet.jpg" alt="Aurélie Bonet, développeuse" class="img-circle">
+            <span class="dev"><blockquote><b>"La vie n'est pas un long un fleuve tranquille."</b></blockquote></span><br/>
+            <p itemprop="description" class="accueiltext-center">Cette phrase a un sens tout particulier pour moi car depuis mon départ de l'Armée, il a fallu que je fasse preuve de <span class="dev"><strong>motivation</strong></span> et de <span class="dev"><strong>persévérance</strong></span>  pour me reconvertir à chaque fois que le besoin s'en faisait ressentir.<br/><br/>
+                Aujourd'hui, mon but est atteint, je suis <span class="dev"><strong>développeuse web</strong></span>  et mon souhait est de <strong>m'épanouir dans ce domaine</strong>.<br/><br/>
+                Mon domaine de compétences s'étend du <span class="dev"><strong>HTML5/CSS3</strong></span> au <span class="dev"><strong>PHP</strong></span>, <span class="dev"><strong>Symfony 2 et 3</strong></span>, <span class="dev"><strong>IONIC</strong></span>, à <span class="dev"><strong>la gestion de projet</strong></span> et à bien d'autres domaines que vous découvrirez si vous consultez <a href="cv.php">mon cv</a>.<br/><br/>
+                Mon profil vous intéresse <a href="contact.php">contactez-moi</a> et poursuivez la visite ...</p>
+        </div>
+    </div>
+</div>
+
+<?php
+include ('footer.php');
+?>
+
+
+<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="script.js"></script>
+<script src="js/bootstrap.min.js"></script>
+</body>
+</html>
