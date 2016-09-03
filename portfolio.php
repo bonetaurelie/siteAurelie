2c2b80db1eb5d1197c0d98088e2e4299dba82b28
bonetaurelie <bonetaurelie@yahoo.fr>
2016-09-03 14:42:25 +0200

    ajout itemscope itemtype itemprop

--- portfolio.php
+++ portfolio.php
@@ -22,7 +22,7 @@
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
 </head>
-<body>
+<body itemscope itemtype="http://schema.org/CreativeWork">
 
 <?php
 include ('menu.php');
@@ -31,18 +31,18 @@ include ('menu.php');
 <div class="container">
     <div class="row">
         <div class="titre">
-            <h1 class="text-center">Mes réalisations:</h1>
+            <h1 itemprop="name" class="text-center">Mes réalisations:</h1>
         </div>
 
         <ul>
-            <li class="projet"><a href="images/Site village/index.html" target="_blank">Mon village Bootstrap</a></li>
-            <li class="projet"><a href="http://www.projetSaint-Didier.fr" target="_blank">Mon village Wordpress</a></li>
-            <li class="projet"><a href="#" target="_blank">Gestionnaire de livres Symfony2/Bootstrap</a></li>
-            <li class="projet"><a href="#" target="_blank">Mon site personnel PHP/Bootstrap/JQuery</a></li>
-            <li class="projet"><a href="http://www.sensabloc.fr" target="_blank">Site sensabloc Wordpress</a></li>
-            <li class="projet"><a href="http://nalo-oc-projet6.herokuapp.com/" target="_blank">Site Nos Amis les Oiseaux (projet en groupe Symfony 3 / Poste d'intégratrice et de Chef de projet)</a></li>
-            <li class="projet"><a href="https://projet-louvre.herokuapp.com/" target="_blank">Billetterie du Louvre Symfony 2</a></li>
-            <li class="projet"><a href="https://github.com/bonetaurelie/projetMultilingua.git" target="_blank">Multilingua Framework IONIC</a></li>
+            <li class="projet"><a itemprop="url" href="images/Site village/index.html" target="_blank">Mon village Bootstrap</a></li>
+            <li class="projet"><a itemprop="url" href="http://www.projetSaint-Didier.fr" target="_blank">Mon village Wordpress</a></li>
+            <li class="projet"><a itemprop="url" href="#" target="_blank">Gestionnaire de livres Symfony2/Bootstrap</a></li>
+            <li class="projet"><a itemprop="url" href="#" target="_blank">Mon site personnel PHP/Bootstrap/JQuery</a></li>
+            <li class="projet"><a itemprop="url" href="http://www.sensabloc.fr" target="_blank">Site sensabloc Wordpress</a></li>
+            <li class="projet"><a itemprop="url" href="http://nalo-oc-projet6.herokuapp.com/" target="_blank">Site Nos Amis les Oiseaux (projet en groupe Symfony 3 / Poste d'intégratrice et de Chef de projet)</a></li>
+            <li class="projet"><a itemprop="url" href="https://projet-louvre.herokuapp.com/" target="_blank">Billetterie du Louvre Symfony 2</a></li>
+            <li class="projet"><a itemprop="url" href="https://github.com/bonetaurelie/projetMultilingua.git" target="_blank">Multilingua Framework IONIC</a></li>
 
         </ul>
     </div>
@@ -53,31 +53,31 @@ include ('menu.php');
             <!-- Wrapper for slides -->
             <div class="carousel-inner" role="listbox">
                 <div class="item active">
-                    <img src="images/mon village bootstrap.PNG" alt="Projet Bootstrap mon village">
+                    <img itemprop="image" src="images/mon village bootstrap.PNG" alt="Projet Bootstrap mon village">
                 </div>
 
                 <div class="item">
-                    <img src="images/accueil%20wpSaintDidier.PNG" alt="Projet Wordpress mon village">
+                    <img itemprop="image" itemprop="image" src="images/accueil%20wpSaintDidier.PNG" alt="Projet Wordpress mon village">
                 </div>
 
                 <div class="item">
-                    <img src="images/gestionnaire.PNG" alt="Projet Symfony2/Bootstrap">
+                    <img itemprop="image" src="images/gestionnaire.PNG" alt="Projet Symfony2/Bootstrap">
                 </div>
 
                 <div class="item">
-                    <img src="images/site perso.PNG" alt="Projet Symfony2/Bootstrap/JQuery">
+                    <img itemprop="image" src="images/site perso.PNG" alt="Projet Symfony2/Bootstrap/JQuery">
                 </div>
 
                 <div class="item">
-                    <img src="images/sensabloc.PNG" alt="Projet Sensabloc">
+                    <img itemprop="image" src="images/sensabloc.PNG" alt="Projet Sensabloc">
                 </div>
 
                 <div class="item">
-                    <img src="images/accueilNAO.PNG" alt="Projet Nos Amis les Oiseaux">
+                    <img itemprop="image" src="images/accueilNAO.PNG" alt="Projet Nos Amis les Oiseaux">
                 </div>
 
                 <div class="item">
-                    <img src="images/accueilLouvre.PNG" alt="Projet Billetterie du Louvre">
+                    <img itemprop="image" src="images/accueilLouvre.PNG" alt="Projet Billetterie du Louvre">
                 </div>
             </div>
             <!-- Controls -->
