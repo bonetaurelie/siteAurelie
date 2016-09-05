f023bad0388591f28ca3398546780c5cbe105615
bonetaurelie <bonetaurelie@yahoo.fr>
2016-09-05 12:36:57 +0200

    retrait du head et insertion d'un include avec le header.php

--- portfolio.php
+++ portfolio.php
@@ -1,27 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="keywords" content=" Aurélie Bonet, portfolio, réalisations, développeuse, chef de projet, chef de projet multimédia, Vaucluse, Provence Alpes Côte d'Azur, PACA, développeur, concepteur, programmeur, conceptrice, programmeuse"/>
-    <meta name="description" content="Vous trouverez ici les projets que j'ai réalisé depuis le début de ma formation."/>
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Aurélie BONET - Mon portfolio</title>
+<html lang="fr">
 
-    <!-- Bootstrap -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
-    <link rel="shortcut icon" type="image/x-icon" href="images/x-icon/faviconBis.ico" />
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="style.css">
+<?php
+include ('header.php');
+?>
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
 <body itemscope itemtype="http://schema.org/CreativeWork">
 
 <?php
