875748806831df86d5301c91d6ec541b00de64aa
bonetaurelie <bonetaurelie@yahoo.fr>
2016-09-09 10:59:09 +0200

    déplacement des liens externes vers le footer

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <!-- Footer -->
 <div id="footer">
     <ul class="copyright">
-        <li>&copy; 2016 Aurélie Bonet</li><li><a href="mentions.php">Mentions légales</a></li>
+        <li>&copy; 2016 Aurélie Bonet</li><li><a href="mentions.php">Mentions légales</a></li><li><a href="https://fr.linkedin.com/in/aurélie-bonet-268aa7b4" target="_blank" class="icon fa-linkedin"></a> <a href="https://github.com/bonetaurelie" target="_blank" class="icon fa fa-github"></a> <a href= skype:aurelie.bonet?add" target="_blank"><i class="fa fa-skype"></i></a></li>
     </ul>
 </div>
